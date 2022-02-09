0b03c986219e433b77d5ddb37dfe195dff6881fa
absonkab <kyab99@gmail.com>
2022-02-09 12:38:07 +0000

    smart token contract and ICO - set token price

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,5 +1,5 @@
 rsync -r src/ docs/
 rsync  build/contracts/* docs/
 git add .
-git commit -m "smart token contract and ICO - set token price calcul"
+git commit -m "smart token contract and ICO - set token price"
 git push -u origin master
