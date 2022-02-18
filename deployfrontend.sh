4b91f632a50802670e216c081c476bdf6c6b7bd7
absonkab <kyab99@gmail.com>
2022-02-18 17:40:22 +0000

    smart token contract and ICO - Convert token price to BNB

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,5 +1,5 @@
 rsync -r src/ docs/
 rsync  build/contracts/* docs/
 git add .
-git commit -m "Texacoin ICO test1"
+git commit -m "smart token contract and ICO - Convert token price to BNB"
 git push -u origin master
